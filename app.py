72f8b1d2db5374f33f0f77a1da6e8c2ba1069f4d
origin <https://github.com/AdsonSantiago/Test-curso-dio-dominando-IDES-java-intellij.git>
2023-04-06 10:49:53 -0300

    commit do api

---
+++ app.py
@@ -0,0 +1,61 @@
+# 1 APi - É um lugar para disponibilizar recursos ou funcionalidades
+# 2 Objetivo - Criar um api que disponibiliza a consulta, criação, edição e exclusão.
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+livros = [
+    {
+        'id': 1,
+        'titulo': 'Senhor dos Aneis - A Sociedade do Anel',
+        'autor': 'J.R.R Tolkien'
+    },
+    {
+        'id': 2,
+        'titulo': 'Harry Potter e a Pedra Filosofal',
+        'autor': 'J.K Howling'
+    },
+    {
+        'id': 3,
+        'titulo': 'Habitos Atomicos',
+        'autor': 'James Clear'
+    }
+]
+
+# Consultar (Todos)
+@app.route('/livros',methods=['GET'])
+def obter_livros():
+    return jsonify(livros)
+
+# Consultar (id)
+@app.route('/livros/<int:id>',methods=['GET'])
+def obter_livros_por_id(id):
+    for livro in livros:
+        if livro.get('id') == id:
+            return jsonify(livro)
+# Editar
+@app.route('/livros/<int:id>',methods=['PUT'])
+def obter_livro_por_id(id):
+    livro_alterado = request.get_json()
+    for indice,livro in enumerate(livros):
+        if livro.get('id') == id:
+            livros[indice].update(livro_alterado)
+# Criar
+@app.route('/livros',methods=['PUT'])
+def incluir_novo_livro():
+    novo_livro = request.get_json()
+    livros.append(novo_livro)
+
+    return jsonify(livros)
+# Excluir
+@app.route('/livros/<int:id>',methods=['DELETE'])
+def excluir_livro(id):
+    for indice, livro in enumerate(livros):
+        if livro.get('id') == id:
+            del livros[indice]
+
+            return jsonify(livros)
+
+if __name__ == '__main__':
+    app.run(port=5000, host='localhost', debug=True)
